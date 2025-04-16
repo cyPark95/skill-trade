7f5e641949c3373dec57bb24ac883e1b3b2eb71c
cyPark95 <parkcy1007@gmail.com>
2025-04-16 17:24:58 +0900

    feat: Initialize Project

---
+++ src/main/java/com/connect/skilltrade/SkillTradeApplication.java
@@ -0,0 +1,13 @@
+package com.connect.skilltrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connect/skilltrade/SkillTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.skilltrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
