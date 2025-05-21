81c91d898f3efa1cf1345af9acd1d14dd1743b37
jo-kim <belongs2jo@gmail.com>
2025-05-21 22:20:46 +0900

    refactor : 경로 변경 및 이름 수정

--- src/main/java/com/connect/skilltrade/domain/auth/UserRepository.java
+++ src/main/java/com/connect/skilltrade/domain/auth/UserRepository.java
@@ -1,4 +1,4 @@
-package com.connect.skilltrade.domain.user;
+package com.connect.skilltrade.domain.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
